c6f2120400bfce6b4c88d373a2bdc3dc0b13df7d
Pablo Lopes <pablolopes@microsoft.com>
2024-07-26 18:25:04 -0300

    Making the refresh improvements in Canvas, organizing files to be more organized.

--- ImageRendering/GameImageRenderer.cs
+++ ImageRendering/GameImageRenderer.cs
@@ -3,91 +3,91 @@ using Microsoft.JSInterop;
 using System;
 using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using UI.ImageRendering;
-
 
 namespace LangtonsAnt
 {
     class GameImageRenderer
     {
-        //public static BitmapSource GetGenerationImageSourceX2(IGame g)
-        public static object GetGenerationImageSourceX2(IJSRuntime js, ElementReference canvas, IGame g)
+        private static IGame? previousGameState;
+
+        public static async Task GetGenerationImageSourceX2(IJSRuntime js, ElementReference canvas, IGame g)
         {
-            // Define parameters used to create the BitmapSource.
-            //PixelFormat pf = PixelFormats.Bgr24;
             int width = g.Size * 2;
             int height = g.Size * 2;
-            int rawStride = 8; //(width * pf.BitsPerPixel + 7) / 8;
-            byte[] rawImage = new byte[rawStride * height];
+            var tasks = new List<Task>();
 
-            // Initialize the image with data.
-            //byte[] pxl_color;
-            var pxl_color = "#000000";
-
-            //clean canvas
-            CleanCanvas(js, canvas);
+            // Not clear canvas if it is the first time
+            if (previousGameState == null)
+            {
+                await CleanCanvas(js, canvas);
+            }
 
             for (int i = 0; i < g.Size; i++)
             {
                 for (int j = 0; j < g.Size; j++)
                 {
-                    // Any ants there?
-                    IAnt? ant = g.Ants.FirstOrDefault(a => (i == a.I) && (j == a.J));
-                    if (ant != null)
+                    // Checking if the pixel needs to be redrawn
+                    // The pixel needs to be redrawn if it is the first time or if the pixel has changed, comparing the current game state with the previous one
+                    bool needsRedraw = previousGameState == null || previousGameState.Field[i, j] != g.Field[i, j] ||
+                                       previousGameState.Ants.Any(a => a.I == i && a.J == j) != g.Ants.Any(a => a.I == i && a.J == j);
+                    
+                    // Redraw the pixel if needed
+                    if (needsRedraw)
                     {
-                        // Draw one of the ants
-                        var headColor = ToHex(Color.Red);
-                        var tailColor = ToHex(Color.Blue);
-                        switch (ant.Direction)
+                        IAnt? ant = g.Ants.FirstOrDefault(a => (i == a.I) && (j == a.J));
+                        if (ant != null)
                         {
-                            case AntDirection.Up:
-                                Draw4in2XScaled(js, canvas, i, j, headColor, tailColor, headColor, tailColor);
-                                break;
-                            case AntDirection.Down:
-                                Draw4in2XScaled(js, canvas, i, j, tailColor, headColor, tailColor, headColor);
-                                break;
-                            case AntDirection.Left:
-                                Draw4in2XScaled(js, canvas, i, j, tailColor, tailColor, headColor, headColor);
-                                break;
-                            case AntDirection.Right:
-                                Draw4in2XScaled(js, canvas, i, j, headColor, headColor, tailColor, tailColor);
-                                break;
+                            var headColor = ToHex(Color.Red);
+                            var tailColor = ToHex(Color.Blue);
+                            switch (ant.Direction)
+                            {
+                                case AntDirection.Up:
+                                    tasks.Add(Draw4in2XScaled(js, canvas, i, j, headColor, tailColor, headColor, tailColor));
+                                    break;
+                                case AntDirection.Down:
+                                    tasks.Add(Draw4in2XScaled(js, canvas, i, j, tailColor, headColor, tailColor, headColor));
+                                    break;
+                                case AntDirection.Left:
+                                    tasks.Add(Draw4in2XScaled(js, canvas, i, j, tailColor, tailColor, headColor, headColor));
+                                    break;
+                                case AntDirection.Right:
+                                    tasks.Add(Draw4in2XScaled(js, canvas, i, j, headColor, headColor, tailColor, tailColor));
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            var tempColor = ColorBytes.ColorSequence[g.Field[i, j]];
+                            var pxl_color = ToHex(tempColor);
+                            tasks.Add(DrawAnhillCell(js, canvas, i, j, pxl_color));
                         }
                     }
-                    else
-                    {
-                        //TODO: Need to implement this!!
-                        var tempColor = ColorBytes.ColorSequence[g.Field[i, j]];
-                        pxl_color = ToHex(tempColor);
-                        DrawAnhillCell(js, canvas, i, j, pxl_color);
-                    }
-
                 }
             }
-            //BitmapSource bitmap = BitmapSource.Create(width, height,
-            //    96, 96, pf, null,
-            //    rawImage, rawStride);
-            //return bitmap;
-            return null;
+
+            await Task.WhenAll(tasks);
+
+            // Update the previous game state
+            previousGameState = g.Clone();
         }
 
         private static async Task Draw4in2XScaled(IJSRuntime js, ElementReference canvas, int i, int j,
-            string colorUpperLeft, string colorLowerLeft, string colorUpperRight, string colorLowerRight){
-
+            string colorUpperLeft, string colorLowerLeft, string colorUpperRight, string colorLowerRight)
+        {
             var w = 0.5;
             var h = 0.5;
 
-            //TODO: could be optimize by drawing to rectangles instead of 4 squares
+            var tasks = new List<Task>
+            {
+                js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i, j, w, h, colorUpperLeft).AsTask(),
+                js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i + w, j, w, h, colorUpperRight).AsTask(),
+                js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i, j + h, w, h, colorLowerLeft).AsTask(),
+                js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i + w, j + h, w, h, colorLowerRight).AsTask()
+            };
 
-            //top left
-            await js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i, j, w, h, colorUpperLeft);
-            //top right
-            await js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i+w, j, w, h, colorUpperRight);
-            //bottom left
-            await js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i, j+h, w, h, colorLowerLeft);
-            //bottom right
-            await js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i+w, j+h, w, h, colorLowerRight);
+            await Task.WhenAll(tasks);
         }
 
         private static async Task DrawAnhillCell(IJSRuntime js, ElementReference canvas, int i, int j, string color)
@@ -98,16 +98,17 @@ namespace LangtonsAnt
             await js.InvokeVoidAsync("LangtonsAnt.drawPixel", canvas, i, j, w, h, color);
         }
 
-        private static async Task CleanCanvas(IJSRuntime js, ElementReference canvas) {
+        private static async Task CleanCanvas(IJSRuntime js, ElementReference canvas)
+        {
             await js.InvokeVoidAsync("LangtonsAnt.clearCanvas", canvas);
         }
 
-        private static String ToHex(System.Drawing.Color c)
+        private static string ToHex(Color c)
         {
             return $"#{c.R:X2}{c.G:X2}{c.B:X2}";
         }
 
-        private static String ToHex(byte[] ca)
+        private static string ToHex(byte[] ca)
         {
             return $"#{ca[2]:X2}{ca[1]:X2}{ca[0]:X2}";
         }
---
+++ wwwroot/LangatonsAnt.js
@@ -0,0 +1,36 @@
+﻿window.LangtonsAnt = {
+    initializeCanvas: function (canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx = canvas.getContext('2d');
+        ctx.fillStyle = '#Ece7d7';
+        ctx.fillRect(10, 10, 500, 500);
+    },
+    drawCanvas: function (canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.arc(Math.floor(Math.random() * (500 + 1)), Math.floor(Math.random() * (500 + 1)), 50, 0, Math.PI * 2, true); // Circle
+        ctx.fillStyle = 'red';
+        ctx.fill();
+    },
+
+    clearCanvas: function (canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.clearRect(10, 10, canvas.width, canvas.height);
+    },
+
+    drawPixel: function (canvas, posX, posY, width, height, color) {
+        let ctx = canvas.getContext('2d');
+        let cubeSize = 20;
+
+        // just to debug
+        //ctx.globalAlpha = 0.25
+
+        posX = posX * cubeSize;
+        posY = posY * cubeSize;
+        width = width * cubeSize;
+        height = height * cubeSize;
+
+        ctx.fillStyle = color;
+        ctx.fillRect(posX, posY, width, height);
+    }
+};
